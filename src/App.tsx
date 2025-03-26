fc024e25f3041e96903c554d4e089339e3f256cd
hanuman reddy <hanumanreddytest@gmail.com>
2025-03-26 11:52:21 +0530

    issue #1: signup

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
-import Signup from "./pages/SignUp";
+import Signup from  "./pages/Signup";
 
 const queryClient = new QueryClient();
 
